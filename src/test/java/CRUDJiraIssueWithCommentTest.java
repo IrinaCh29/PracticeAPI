47839e8bd9332b6662c2bebd2beaa801e87fb07c
Irina <irina.chub29@gmail.com>
2020-09-06 21:14:11 +0300

    separating LR29HW44 and LR30HW45 into 2 different classes

--- src/test/java/CRUDJiraIssueWithCommentTest.java
+++ src/test/java/CRUDJiraIssueWithCommentTest.java
@@ -6,8 +6,9 @@ import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 
 
-public class CRUDJiraIssueTest {
+public class CRUDJiraIssueWithCommentTest {
   private String ticketId;
+  private String commentId;
 
   @Test
   public void crudNewIssue() {
@@ -54,6 +55,62 @@ public class CRUDJiraIssueTest {
     getIssueResponse.print();
     assertEquals(getIssueResponse.path("fields.summary"), "Test summary API ticket");
     assertEquals(getIssueResponse.path("fields.creator.name"), "IrinaChub");
+
+    //Add comment and status is 201
+    Response postCommentInIssueResponse =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            body("{\n" +
+                "    \"body\":\"Test API added comment\"}").
+            when().
+            post("https://jira.hillel.it/rest/api/2/issue/" + ticketId + "/comment").
+            then().
+            contentType(ContentType.JSON).
+            statusCode(201).
+            extract().response();
+    postCommentInIssueResponse.print();
+    commentId = postCommentInIssueResponse.path("id");
+    System.out.println(commentId);
+
+    //Get added comment and status is 200
+    Response getCommentResponseInIssue =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            when().
+            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId + "/comment/" + commentId).
+            then().
+            extract().response();
+    getCommentResponseInIssue.print();
+    assertEquals(getCommentResponseInIssue.statusCode(), 200);
+    assertEquals(getCommentResponseInIssue.path("body"), "Test API added comment");
+    assertEquals(getCommentResponseInIssue.path("author.name"), "IrinaChub");
+
+    //Delete added comment and status is 204
+    Response deleteAddedCommentInIssueResponse =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            when().
+            delete("https://jira.hillel.it/rest/api/2/issue/" + ticketId + "/comment/" + commentId).
+            then().
+            statusCode(204).
+            extract().response();
+    deleteAddedCommentInIssueResponse.print();
+
+    //Get issue with with Not Found Comment and status is 404
+    Response getIssueNotFoundCommentResponse =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            when().
+            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId + "/comment/" + commentId).
+            then().
+            contentType(ContentType.JSON).
+            statusCode(404).
+            extract().response();
+    getIssueNotFoundCommentResponse.print();
 
     //Delete created issue and status is 204
     Response deleteIssueResponse =
