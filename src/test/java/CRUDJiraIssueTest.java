d339b9907fbe562618b117735b9fdf0d01869213
Irina <irina.chub29@gmail.com>
2020-09-06 21:08:24 +0300

    LR30HW45

--- src/test/java/CRUDJiraIssueTest.java
+++ src/test/java/CRUDJiraIssueTest.java
@@ -8,11 +8,12 @@ import static org.testng.Assert.assertEquals;
 
 public class CRUDJiraIssueTest {
   private String ticketId;
+  private String commentId;
 
   @Test
   public void crudNewIssue() {
-    //POST - create new issue
-    Response postResponse =
+    //POST - create new issue and status is 201
+    Response postIssueResponse =
         given().
             auth().preemptive().basic("IrinaChub", "IrinaChub").
             contentType(ContentType.JSON).
@@ -37,11 +38,11 @@ public class CRUDJiraIssueTest {
             contentType(ContentType.JSON).
             statusCode(201).
             extract().response();
-    ticketId = postResponse.path("id");
+    ticketId = postIssueResponse.path("id");
     System.out.println(ticketId);
 
-    //Get created issue
-    Response getResponse =
+    //Get created issue and status is 200
+    Response getIssueResponse =
         given().
             auth().preemptive().basic("IrinaChub", "IrinaChub").
             contentType(ContentType.JSON).
@@ -51,33 +52,88 @@ public class CRUDJiraIssueTest {
             contentType(ContentType.JSON).
             statusCode(200).
             extract().response();
-    getResponse.print();
-    assertEquals(getResponse.path("fields.summary"), "Test summary API ticket");
-    assertEquals(getResponse.path("fields.creator.name"), "IrinaChub");
+    getIssueResponse.print();
+    assertEquals(getIssueResponse.path("fields.summary"), "Test summary API ticket");
+    assertEquals(getIssueResponse.path("fields.creator.name"), "IrinaChub");
 
+    //Add comment and status is 201
+    Response postCommentInIssueResponse =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            body("{\n" +
+                "    \"body\":\"Test API added comment\"}").
+            when().
+            post("https://jira.hillel.it/rest/api/2/issue/" + ticketId + "/comment").
+            then().
+            contentType(ContentType.JSON).
+            statusCode(201).
+            extract().response();
+    postCommentInIssueResponse.print();
+    commentId = postCommentInIssueResponse.path("id");
+    System.out.println(commentId);
 
-//    //Delete created issue and status is 204
-//    Response deleteIssueResponse =
-//        given().
-//            auth().preemptive().basic("IrinaChub", "IrinaChub").
-//            contentType(ContentType.JSON).
-//            when().
-//            delete("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
-//            then().
-//            statusCode(204).
-//            extract().response();
-//    deleteIssueResponse.print();
-//
-//    //Get deleted issue and status is 404
-//    Response getDeletedIssueResponse =
-//        given().
-//            auth().preemptive().basic("IrinaChub", "IrinaChub").
-//            contentType(ContentType.JSON).
-//            when().
-//            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
-//            then().
-//            statusCode(404).
-//            extract().response();
-//    getDeletedIssueResponse.print();
+    //Get added comment and status is 200
+    Response getCommentResponseInIssue =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            when().
+            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId + "/comment/" + commentId).
+            then().
+            extract().response();
+    getCommentResponseInIssue.print();
+    assertEquals(getCommentResponseInIssue.statusCode(), 200);
+    assertEquals(getCommentResponseInIssue.path("body"), "Test API added comment");
+    assertEquals(getCommentResponseInIssue.path("author.name"), "IrinaChub");
+
+    //Delete added comment and status is 204
+    Response deleteAddedCommentInIssueResponse =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            when().
+            delete("https://jira.hillel.it/rest/api/2/issue/" + ticketId + "/comment/" + commentId).
+            then().
+            statusCode(204).
+            extract().response();
+    deleteAddedCommentInIssueResponse.print();
+
+    //Get issue with with Not Found Comment and status is 404
+    Response getIssueNotFoundCommentResponse =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            when().
+            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId + "/comment/" + commentId).
+            then().
+            contentType(ContentType.JSON).
+            statusCode(404).
+            extract().response();
+    getIssueNotFoundCommentResponse.print();
+
+    //Delete created issue and status is 204
+    Response deleteIssueResponse =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            when().
+            delete("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
+            then().
+            statusCode(204).
+            extract().response();
+    deleteIssueResponse.print();
+
+    //Get deleted issue and status is 404
+    Response getDeletedIssueResponse =
+        given().
+            auth().preemptive().basic("IrinaChub", "IrinaChub").
+            contentType(ContentType.JSON).
+            when().
+            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
+            then().
+            statusCode(404).
+            extract().response();
+    getDeletedIssueResponse.print();
   }
 }
